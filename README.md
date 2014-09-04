40aa48b2bed30b463d109e7f18836a86e6666e33
lloydtorres <lloyd.torres95@gmail.com>
2014-09-04 00:22:23 -0400

    random-scripts
    Scripts for various problems.

---
+++ README.md
@@ -0,0 +1,13 @@
+random-scripts
+==============
+
+Scripts I've written to deal with various problems. Mostly written in Python.
+
+JSON_Formatter.py
+-----------------
+
+Takes in a '.json' file containing a one-liner JSON file and creates a new file that's nicely formatted.
+
+Run from command line/terminal using:
+
+'$PATH/JSON_Formatter.py  <path of JSON file>'
---
+++ src/JSON_Formatter.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+
+# JSON Formatter, by Lloyd Torres
+# Quick way of taking a JSON file written in one line
+# and formatting it nicely. Licensed under MIT License.
+
+import sys # sys used for command line arguments
+
+bracketLevel = 1 # amount of tabs a line needs
+
+# used to format entries that have other objects in them
+def recursiveBracket(toRead):
+    global bracketLevel
+
+    toRead = toRead.split("{",1)
+    outFile.write("\t"*bracketLevel + toRead[0].strip() + " {\n")
+    bracketLevel += 1
+    
+    if toRead[1].find("{") != -1: # means another object is in entry
+        recursiveBracket(toRead[1])
+    else:
+        outFile.write("\t"*bracketLevel + toRead[1].strip() + ",\n")
+
+if len(sys.argv) != 0: # Make sure there's command line arguments
+    checkArg = sys.argv[1].strip()
+    if checkArg[len(checkArg)-5:] == ".json": # Make sure argument is actually json file
+        # Open file in argument
+        inFile = open(checkArg)
+        fileContents = inFile.readline().split(",")
+        inFile.close()
+
+        # Remove first instance of {, last instance of }
+        for f in range(0,len(fileContents)):
+            if fileContents[f].find("{") != -1:
+                fileContents[f] = fileContents[f].replace("{","",1)
+                break
+
+        # Clever solution from:
+        # http://stackoverflow.com/questions/2556108/how-to-replace-the-last-occurence-of-an-expression-in-a-string
+        for l in range(len(fileContents)-1,-1,-1):
+            if fileContents[l].find("}") != -1:
+                fileContents[l] = fileContents[l].replace("}","",1)
+                break
+
+        # Open write file
+        name = checkArg[:len(checkArg)-5] # get rid of extension
+        
+        outFile = open(name + "_format.json","w")
+        outFile.write("{\n")
+
+        # Actual processing
+        for entry in fileContents:
+            entry = entry.strip()
+            
+            if entry.find("{") != -1: # leading brackets
+                recursiveBracket(entry)
+            elif entry.find("}") != -1: # closing brackets
+                noBrackets = entry.count("}")
+                entry = entry.replace("}","")
+                
+                outFile.write("\t"*bracketLevel + entry + "\n")
+                for i in range(0,noBrackets): # sets closing brackets
+                    bracketLevel -= 1
+                    outFile.write("\t"*bracketLevel + "}\n")
+            else:
+                outFile.write("\t"*bracketLevel + entry + ",\n")
+
+        outFile.write("}")
+        outFile.close()
+
+    else:
+        print("ERROR: Not a JSON file.")
+else:
+    print("ERROR: No arguments found.")
+        
